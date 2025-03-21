3e283d965407564836fa50fd615998b2e5e0370b
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-21 15:17:16 -0600

    like

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,10 +18,20 @@
     </div>
 
     <!-- Contenido del Post -->
-    <div class="w-full mt-4 p-3 bg-white rounded-lg shadow">
+    <div class="w-full mt-4 p-3 bg-white rounded-lg shadow items-center gap-0">
         
-        <div class="mt-4">
-            <p class="font-semibold text-gray-600">0 Likes</p>
+        <div class="p-3 flex items-center gap-4">
+        <form action="">
+            <div class="flex items-center gap-2">
+                <button type="button">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>              
+                </button>
+                </div>
+        </form>
+            <p >0 Likes</p>
         </div>
         
 
