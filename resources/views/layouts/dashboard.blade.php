c820a0808f5377f0679ff16209b4ec60f10c16a7
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-23 06:04:39 -0600

    edit terminado y n posts

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,7 +10,12 @@ Perfil: {{ $user->username }}
         <div class="flex justify-center">
             <div class="w-full md:8/12 lg:w-6/12 flex flex-col items-center md:items-start md:flex-row  py-10 md:py-10">
                 <div class="w-8/12 lg:w-6/12 px-5">
-              <img src="{{ asset('img/usuario.svg') }}" alt="img user">
+            <img src="{{ 
+                $user -> imagen? 
+                asset('perfiles'. '/'. $user->imagen) :  
+                asset('img/usuario.svg') }}" 
+                alt="img user">
+    
                 </div>
 
 
@@ -47,7 +52,7 @@ Perfil: {{ $user->username }}
                     0
                     <span class="font-normal ">Siguiendo</span>
                     <p class="text-gray-800 text-sm mb-3 font-semibold">
-                        0
+                        {{ $user->posts->count() }}
                         <span class="font-normal ">Posts</span>
                 </div>
 
